6a60d2f5a01f60e934cce402e3988997a8e146d4
NoUser <NoUser@PC30K>
2022-07-09 15:22:54 -0300

    Adicionar arquivos de projeto.

---
+++ Mostrar_1_a_50/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mostrar_1_a_50
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numerosP;
+            for (numerosP = 0; numerosP < 51; numerosP++)
+            {
+                Console.WriteLine(numerosP);
+            }
+            Console.ReadKey();
+        }
+    }
+}
